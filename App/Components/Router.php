de3f9f0b2bdc39ee6dce10b13082b9c390c83d3d
Illia <mor1l@yandex.ua>
2017-01-20 17:36:08 +0200

    change register validation

--- App/Components/Router.php
+++ App/Components/Router.php
@@ -4,13 +4,14 @@ namespace App\Components;
  * Class Router
  * Component to operate the routes
  */
-class Router {
+class Router
+{
 
-	/**
-	 * Property to hold an array of routes
-	 * @var array
-	 */
-	private $routes;
+    /**
+     * Property to hold an array of routes
+     * @var array
+     */
+    private $routes;
 
     /**
      * Property to hold a namespace for controllers
@@ -18,63 +19,66 @@ class Router {
      */
     private $controlNameSpace;
 
-	/**
-	 * Constructor
-	 */
-	public function __construct($controlNameSpace) {
+    /**
+     * Constructor
+     */
+    public function __construct($controlNameSpace)
+    {
 
-		$routesPath = ROOT . '/App/Config/routes.php';
+        $routesPath = ROOT . '/App/Config/routes.php';
 
         $this->controlNameSpace = $controlNameSpace;
 
-		$this -> routes =
-		include ($routesPath);
-	}
+        $this->routes =
+            include($routesPath);
+    }
 
-	/**
-	 * Returns the query string
-	 */
-	private function getURI() {
-		if (!empty($_SERVER['REQUEST_URI'])) {
-			return trim($_SERVER['REQUEST_URI'], '/');
-		}
-	}
+    /**
+     * Returns the query string
+     */
+    private function getURI()
+    {
+        if (!empty($_SERVER['REQUEST_URI'])) {
+            return trim($_SERVER['REQUEST_URI'], '/');
+        }
+    }
 
-	/**
-	 * Method to handle the request
-	 */
-	public function run() {
+    /**
+     * Method to handle the request
+     */
+    public function run()
+    {
 
-		$uri = $this -> getURI();
+        $uri = $this->getURI();
 
-		// Check the availability of this request in the array of routes (routesOld.php)
-		foreach ($this->routes as $uriPattern => $path) {
+        // Check the availability of this request in the array of routes (routesOld.php)
+        foreach ($this->routes as $uriPattern => $path) {
 
-			// Compare $uriPattern and $uri
-			if (preg_match("~$uriPattern~", $uri)) {
+            // Compare $uriPattern and $uri
+            if (preg_match("~$uriPattern~", $uri)) {
 
                 $internalRoute = preg_replace("~$uriPattern~", $path, $uri);
 
 
                 $segments = explode('/', $internalRoute);
 
-				$controllerName = array_shift($segments) . 'Controller';
-				$controllerName = ucfirst($controllerName);
+                $controllerName = array_shift($segments) . 'Controller';
+                $controllerName = ucfirst($controllerName);
 
-				$action = array_shift($segments);
-				$actionName = 'action' . ucfirst($action);
+                $action = array_shift($segments);
+                $actionName = 'action' . ucfirst($action);
 
-				$parameters = $segments;
+                $parameters = $segments;
 
-                $controllerName = $this->controlNameSpace.$controllerName;
+                $controllerName = $this->controlNameSpace . $controllerName;
                 $controllerObject = new $controllerName;
 
                 return ['ref' => $controllerObject,
-                        'actionName' => $actionName,
-                        'args' => $parameters];
+                    'actionName' => $actionName,
+                    'args' => $parameters];
 
-			}
-		}
-	}
+            }
+        }
+    }
 
 }
