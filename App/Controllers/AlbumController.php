c47be3bde101ecf016b599b57842bcce8fe7973b
Orange-Men <Vadim1996XC@ukr.net>
2017-01-23 15:54:49 +0200

    Added validation photo, uploading multiples photos

--- App/Controllers/AlbumController.php
+++ App/Controllers/AlbumController.php
@@ -2,7 +2,7 @@
 namespace App\Controllers;
 
 use App\Models\{
-    Album, AlbumPhoto, AlbumUser, User
+    Album, AlbumPhoto, User
 };
 
 class AlbumController extends PageController
@@ -25,24 +25,25 @@ class AlbumController extends PageController
     public function actionInsertPhoto($id)
     {
         $this->userId = User::checkLogged();
-
-        if (!isset($_FILES['uploadPhoto']) || $_FILES['uploadPhoto']['error'] != 0) {
-            header('Location: /album/' . $id);
+        $count = count($_FILES['uploadPhoto']['name']);
+        if (!isset($_FILES['uploadPhoto'])) {
+            throw new \Exception('Photo required');
         }
-
-        $path = ROOT . '/public/photos/'; // директория для загрузки
-        $ext = explode('.',$_FILES['uploadPhoto']['name']); // расширение
-        $newName = md5(time() . $ext[0]) . '.' . $ext[1]; // новое имя с расширением
-        $full_path = $path . $newName; // полный путь с новым именем и расширением
-
-        if(!move_uploaded_file($_FILES['uploadPhoto']['tmp_name'], $full_path)){
-            throw new \Exception('Can\'t save file');
+        for ($i = 0; $i < $count; $i++) {
+            $this->fileValidation($i);
+            $path = ROOT . '/public/photos/';
+            $ext = explode('.', $_FILES['uploadPhoto']['name'][$i]);
+            $newName = md5(time() . $ext[0]) . '.' . $ext[1];
+            $full_path = $path . $newName;
+            if (!move_uploaded_file($_FILES['uploadPhoto']['tmp_name'][$i], $full_path)) {
+                header('Location: /album/' . $id);
+            }
+            $newPhoto = new AlbumPhoto();
+            $newPhoto->fileName = $newName;
+            $newPhoto->albumId = $id;
+            $newPhoto->status = 'active';
+            $newPhoto->insert();
         }
-        $newPhoto = new AlbumPhoto();
-        $newPhoto->fileName = $newName;
-        $newPhoto->albumId = $id;
-        $newPhoto->status = 'active';
-        $newPhoto->insert();
         header('Location: /album/' . $id);
     }
 
@@ -64,5 +65,18 @@ class AlbumController extends PageController
         $userAlbum->update();
         
         header('Location: /album/' . $id);
+    }
+
+    private function fileValidation($i)
+    {
+        $size = 1024 * 3 * 1024;//3 Mb
+        $allowedExts = ['jpg', 'jpeg', 'gif', 'png'];
+        $ext = explode('.', $_FILES['uploadPhoto']['name'][$i]);
+        if ($_FILES['uploadPhoto']['size'][$i] > $size
+            || $_FILES['uploadPhoto']['error'][$i] != 0
+            || !in_array(end($ext), $allowedExts)
+        ) {
+            throw new \Exception('Size > 3 Mb OR some error occurred OR unsupported format');
+        }
     }
 }
